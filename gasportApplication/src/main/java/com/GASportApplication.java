9086eabdfb7fed1d8a28e39b0e91c266b439d550
pachauri <vipulpachauri12@gmail.com>
2018-04-21 23:57:14 +0530

    Log4j2 logger added

--- gasportApplication/src/main/java/com/GASportApplication.java
+++ gasportApplication/src/main/java/com/GASportApplication.java
@@ -1,17 +1,31 @@
 package com;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class GASportApplication {
+public class GASportApplication implements ApplicationRunner{
+
+    private static Logger logger = LoggerFactory.getLogger(GASportApplication.class);
 
     public static void main(String[] args) {
 
         SpringApplication.run(GASportApplication.class, args);
         System.out.println("Server started working !!");
+    }
+
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        logger.debug("Debugging log");
+        logger.info("Info log");
+        logger.warn("Hey, This is a warning!");
+        logger.error("Oops! We have an Error. OK");
     }
 
     @Bean
